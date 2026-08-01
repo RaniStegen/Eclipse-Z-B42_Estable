17ff7bed3dd042066bf0d3eae2c3255bb539f29a
RaniStegen <245702837+RaniStegen@users.noreply.github.com>
2026-08-01 02:59:55 +0000

    Lifestyle: normalizar volumen y alcance de instrumentos

--- EclipseZ - 1/Contents/mods/ECZ_4/common/media/lua/shared/TimedActions/PlayInstrumentTraining.lua
+++ EclipseZ - 1/Contents/mods/ECZ_4/common/media/lua/shared/TimedActions/PlayInstrumentTraining.lua
@@ -242,8 +242,8 @@ function PlayInstrumentTraining:learnMidSong()
 end
 
 function PlayInstrumentTraining:soundPing()
-	local soundRadius, volume = 10, 5
-	if self.character:isOutside() then soundRadius, volume = 30, 10; end
+	local soundRadius, volume = 8, 3
+	if self.character:isOutside() then soundRadius, volume = 20, 6; end
 	addSound(self.character,self.character:getX(),self.character:getY(),self.character:getZ(),soundRadius,volume)
 end
 
