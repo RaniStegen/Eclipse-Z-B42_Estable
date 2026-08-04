8a3ef6f4481bfde6544e6c22ce7fbcb0e5e30550
Eclipse-Z Diagnóstico <actions@users.noreply.github.com>
2026-08-04 20:50:37 +0000

    Eliminar diagnósticos temporales de equipamiento

--- MODPACK-ACTUAL-EN-SERVIDOR/_AUDITORIA/TEMP_DIAGNOSTICO_WEAR_ACTION_V4/archivos/_AUDITORIA/TEMP_DIAGNOSTICO_WEAR_ACTION_V4/archivos/EclipseZ - 2/Contents/mods/ECZ2_7.1/42.18/media/registries.lua
+++
@@ -1,7 +0,0 @@
-
-SpnOpenCloth = {}
-SpnOpenCloth.ItemBodyLocation = {}
-
-SpnOpenCloth.ItemBodyLocation.JACKET_OPEN = ItemBodyLocation.register("SpnOpenCloth:JACKET_OPEN")
-SpnOpenCloth.ItemBodyLocation.JACKET_ROLL = ItemBodyLocation.register("SpnOpenCloth:JACKET_ROLL")
-SpnOpenCloth.ItemBodyLocation.JACKET_OPENROLL = ItemBodyLocation.register("SpnOpenCloth:JACKET_OPENROLL")
